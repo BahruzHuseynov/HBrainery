e4a0879705a85585b549772c903823a9491baeaa
Bahruz <83864206+BahruzHuseynov@users.noreply.github.com>
2024-06-19 21:21:02 +0400

    Create Python_Ders_9.py

---
+++ Python Proqramlaşdırma Dərsləri/Python_Ders_9.py
@@ -0,0 +1,18 @@
+x = input() # x = "792"
+print(x * 2) # 792792
+
+x = input() # x = "792"
+x = int(x) # x = 792
+print(x * 2) # 792 * 2 = 1584
+
+x = int(input("Ədədi daxil edin: "))
+print(x * 2)
+
+x = int(input()) # x = 2
+y = int(input()) # x = 3
+print(x + y)
+
+x = input().split() # "2 3".split() -> ["2", "3"]
+a = int(x[0]) # 2
+b = int(x[1]) # 3
+print(a + b)
